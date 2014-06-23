2c18915ddbdaac4ee39f6b3a04f7fd6b4cae9e16
Du Bois <eloi.du.bois@gmail.com>
2014-06-23 22:03:22 +0200

    judymatch: added assert on sizes.

--- libraries/judymatch/src/judymatch/geometry/distance.hpp
+++ libraries/judymatch/src/judymatch/geometry/distance.hpp
@@ -13,12 +13,7 @@ namespace geometry
 template<class Vec>
 const double euclidean_distance( const Vec & a, const Vec & b )
 {
-    if( a.size() != b.size() )
-    {
-        std::cerr << "oulah" << std::endl;
-        std::string * x = (std::string*)1248912832;
-        std::cout << *x << std::endl;;
-    }
+    assert( a.size() == b.size() );
     double sum = 0.0;
     for( std::size_t i = 0; i < a.size(); ++i )
     {
