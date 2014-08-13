da97e57f8804a9ab161a6ae3528ab30d6a7eacec
Du Bois <eloi.du.bois@gmail.com>
2014-08-13 19:27:53 +0200

    judymatch: using templates for dct function (to handle different kind of vectors)

--- libraries/judymatch/src/judymatch/lsh/dct/dct.hpp
+++ libraries/judymatch/src/judymatch/lsh/dct/dct.hpp
@@ -14,12 +14,91 @@ namespace lsh
 namespace dct
 {
 
+using namespace boost::numeric;
+
 /**
  * Compute the 1D dct of a given signal
  * @param vecIn given signal
  * @param vecOut output dct coefficients
  */
-void dct( const judymatch::vec_t & vec_in, judymatch::vec_t & vec_out );
+template<class V>
+void dct( const V & vec_in, V & vec_out )
+{
+    using namespace boost::math;
+    using namespace boost::math::constants;
+    BOOST_ASSERT( vec_in.size() );
+    BOOST_ASSERT( vec_out.size() );
+    const double k_pi_vec_sz = pi<double>() / ( vec_in.size() );
+
+    for( std::size_t i = 0; i < vec_in.size(); ++i )
+    {
+        vec_out[i] = 0.0;
+        for( std::size_t j = 0; j < vec_in.size(); ++j )
+        {
+            vec_out[i] += vec_in[j] * std::cos( k_pi_vec_sz * ( j + 0.5 ) * ( i ) );
+        }
+    }
+}
+
+/**
+ * Compute the 1D dct of a given string
+ * @param str_in given string
+ * @param str_out output dct coefficients shrinked into string chars
+ */
+template<class T>
+void dct_string( const T & str_in, T & str_out )
+{
+    using namespace boost::math;
+    using namespace boost::math::constants;
+    const double k_pi_vec_sz = pi<double>() / ( str_in.length() );
+
+    for( std::size_t i = 0; i < str_in.length(); ++i )
+    {
+        double v = 0;
+        for( std::size_t j = 0; j < str_in.length(); ++j )
+        {
+            v += str_in[j] * std::cos( k_pi_vec_sz * ( j + 0.5 ) * ( i ) );
+        }
+        const char *doubleData = reinterpret_cast<const char*>( &v );
+        for( std::size_t k = 0; k < sizeof(double); ++k )
+        {
+            str_out += doubleData[i];
+        }
+    }
+}
+
+template<class T>
+class dct_1d_lsh_string
+{
+public:
+    typedef T value_type;
+
+public:
+    dct_1d_lsh_string() {}
+
+    /**
+     * @brief Compute the 1D location dependant lsh of a given signal
+     * @param vec_in given signal
+     * @return an lsh (index giving an order relation)
+     */
+    const value_type hash_for_construction( const value_type & vec_in ) const
+    {
+        value_type vec_out;
+        dct_string( vec_in, vec_out );
+        return vec_out;
+    }
+
+    /**
+     * @brief Compute the 1D location independant lsh of a given string
+     * @param vec_in given string
+     * @return an lsh (index giving an order relation)
+     */
+    const value_type hash_for_find_back( const value_type & vec_in ) const
+    { return hash_for_construction( vec_in ); }
+
+    inline static const std::size_t hash_size()
+    { return 0; } // Dynamic
+};
 
 template<std::size_t S>
 class dct_1d_lsh
